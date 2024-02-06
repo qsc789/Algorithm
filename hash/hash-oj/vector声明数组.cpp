5ecbb5abd735ddd8f7b166688f69cb1acdcecf92
qsc789 <1279495882@qq.com>
2024-02-06 14:08:07 +0800

    vector声明加初始化

---
+++ hash/hash-oj/vector声明数组.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int num1=s.size();//s.length也行
+        int num2=t.size();
+        if(num1!=num2)
+        {
+            return false;
+        }
+        vector<int> table(26,0);//数组26个元素，初始化为0
+        for (auto& ch: s) //auto自动匹配类型（遍历）
+        {
+            table[ch - 'a']++;
+        }
+        for (auto& ch: t)
+        {
+            table[ch - 'a']--;
+            if (table[ch - 'a'] < 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
