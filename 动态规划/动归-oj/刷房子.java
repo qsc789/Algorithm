7c84b141d16795e68d61a21c667b92d619146c4f
qsc789 <1279495882@qq.com>
2024-02-12 22:23:14 +0800

    线性dp

---
+++ 动态规划/动归-oj/刷房子.java
@@ -0,0 +1,20 @@
+//leetcode LCR 091
+//链接：https://leetcode.cn/problems/JEj789/solutions/1621250/fen-shua-fang-zi-by-leetcode-solution-q0kh/
+class Solution {
+    public int minCost(int[][] costs) {
+        int n = costs.length;
+        int[] dp = new int[3];
+        for (int j = 0; j < 3; j++) {
+            dp[j] = costs[0][j];
+        }
+        for (int i = 1; i < n; i++) {
+            int[] dpNew = new int[3];
+            for (int j = 0; j < 3; j++) {
+                dpNew[j] = Math.min(dp[(j + 1) % 3], dp[(j + 2) % 3]) + costs[i][j];
+            }
+            dp = dpNew;
+        }
+        return Arrays.stream(dp).min().getAsInt();
+    }
+}
+
