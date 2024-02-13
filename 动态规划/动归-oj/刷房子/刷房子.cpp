f1507ef8dcfae180b7211eb92958d52ce20e7264
qsc789 <1279495882@qq.com>
2024-02-13 21:28:35 +0800

    线性dp

--- 动态规划/动归-oj/刷房子/刷房子.cpp
+++ 动态规划/动归-oj/刷房子/刷房子.cpp
@@ -21,6 +21,7 @@ public:
             }
             dp=Newdp;
         }
-        return *min_element(dp.begin(),dp.end());
+        return *min_element(dp.begin(),dp.end());//algorithm头文件接口函数
+        //https://blog.csdn.net/weixin_38505045/article/details/88656453?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522170782990816800182113756%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=170782990816800182113756&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~top_positive~default-1-88656453-null-null.142^v99^pc_search_result_base7&utm_term=min_element&spm=1018.2226.3001.4187
     }
 };
