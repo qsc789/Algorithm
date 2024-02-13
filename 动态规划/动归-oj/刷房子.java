6daadbdf2e8147c8281bc18ac928706b342c53a2
qsc789 <1279495882@qq.com>
2024-02-13 20:27:46 +0800

    线性dp

--- 动态规划/动归-oj/刷房子.java
+++ 动态规划/动归-oj/刷房子.java
@@ -5,16 +5,16 @@ class Solution {
         int n = costs.length;
         int[] dp = new int[3];
         for (int j = 0; j < 3; j++) {
-            dp[j] = costs[0][j];
+            dp[j] = costs[0][j];//第一个房子dp，等于costs
         }
         for (int i = 1; i < n; i++) {
-            int[] dpNew = new int[3];
+            int[] dpNew = new int[3];//每个房子都有一个dp
             for (int j = 0; j < 3; j++) {
                 dpNew[j] = Math.min(dp[(j + 1) % 3], dp[(j + 2) % 3]) + costs[i][j];
             }
-            dp = dpNew;
+            dp = dpNew;//存储
         }
-        return Arrays.stream(dp).min().getAsInt();
+        return Arrays.stream(dp).min().getAsInt();//返回第n个房子三种颜色中最小的
     }
 }
 
