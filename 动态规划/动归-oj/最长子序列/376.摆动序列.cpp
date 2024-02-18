e00c60d7c38dbda3c68916f3c6f6510ff03b9d3a
qsc789 <1279495882@qq.com>
2024-02-18 16:27:13 +0800

    两个dp，上升序列和下降序列

---
+++ 动态规划/动归-oj/最长子序列/376.摆动序列.cpp
@@ -0,0 +1,35 @@
+//leetcode376
+//https://leetcode.cn/problems/wiggle-subsequence/
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int num=nums.size();
+        if(num<2)
+        {
+            return num;
+        }
+        vector<int> up(num),down(num);
+        up[0]=down[0]=1;
+        for(int i=1;i<num;i++)
+        {
+            if(nums[i]>nums[i-1])//上升
+            {
+                up[i]=max(up[i-1],down[i-1]+1);//i-1的上升或i-1的下降加1
+                down[i]=down[i-1];
+            }
+            else if(nums[i]<nums[i-1])//下降
+            {
+                up[i]=up[i-1];
+                down[i]=max(down[i-1],up[i-1]+1);//i-1的下降或i-1的上升加1
+            }
+            else
+            {
+                up[i]=up[i-1];
+                down[i]=down[i-1];
+            }
+        }
+
+        return max(up[num-1],down[num-1]);
+
+    }
+};
