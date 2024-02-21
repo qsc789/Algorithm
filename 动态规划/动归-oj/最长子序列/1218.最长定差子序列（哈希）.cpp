b1dc03c1ea11dafcedb612e95b1fcb82e13d96fc
qsc789 <1279495882@qq.com>
2024-02-21 19:52:51 +0800

    草率的unordered_map

--- 动态规划/动归-oj/最长子序列/1218.最长定差子序列（哈希）.cpp
+++ 动态规划/动归-oj/最长子序列/1218.最长定差子序列（哈希）.cpp
@@ -5,9 +5,10 @@ public:
     int longestSubsequence(vector<int>& arr, int difference) {
         int ans=0;
         unordered_map<int,int> dp;//复杂哈希表，int，int型键值对
+        //空的map
         for(int i:arr)
         {
-            dp[i]=dp[i-difference]+1;
+            dp[i]=dp[i-difference]+1;//下标问题未知
             ans=max(ans,dp[i]);
         }
         return ans;
