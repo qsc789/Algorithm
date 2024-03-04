5969c3b20f56b666fdb96c094673c742612da4a1
qsc789 <1279495882@qq.com>
2024-03-04 12:00:36 +0800

    n个数所有排列情况

--- 枚举/枚举模版/排列型枚举.cpp
+++ 枚举/枚举模版/排列型枚举.cpp
@@ -1,35 +1,81 @@
-
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
 int n;
+int s[1000];
+int order[100];
+bool chosen[100];
+void calc(int k)
+{
+	if (k == n + 1)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			cout << order[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (chosen[s[i]])
+			{
+				continue;
+			}
+		chosen[s[i]] = true;
+		order[k] = s[i];
+		calc(k + 1);
+		chosen[s[i]] = false;
+		order[k] = 0;
+		}
+		
+	}
+
+
+}
+int main()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> s[i];
+	}
+	calc(1);
+	return 0;
+}
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int n; //共计N个数
 int order[20];
 bool chosen[20];
 void calc(int k)
 {
-	if(k==n+1)
-	{
-		for(int i=0;i<sizeof(order)/sizeof(order[0]);i++)
-		{
-			if(order[i]!=0)
-			cout<<order[i]<<" ";		
-		}
-	}
-	for(int i=1;i<=n;i++)
-	{
-		if(chosen[i])
-		{
-			continue;
-		}
-		order[k]=i;
-		chosen[i]=1;
-		calc(k+1);
-		chosen[i]=0;
-		order[k]=0;
-	 } 
+   if (k == n + 1)
+   {
+       for (int i = 1; i <= n; i++)
+           cout << order[i] << " ";
+
+       puts("");
+
+       return;
+   }
+   for (int i = 1; i <= n; i++)
+   {
+       if (chosen[i])
+           continue;
+       order[k] = i;
+       chosen[i] = 1;
+       calc(k + 1);
+       chosen[i] = 0;
+       order[k] = 0;
+   }
 }
 int main()
 {
-	cin>>n;
-	calc(1);
-	return 0;
+   cin >> n;
+   calc(1);
 }
