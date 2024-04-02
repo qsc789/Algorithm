85734176ab1b0ec9c19d2de13d37ebc3acb1bd75
qsc789 <1279495882@qq.com>
2024-04-02 10:50:53 +0800

    按位与快幂

---
+++ 数论/取模与快幂/快幂.cpp
@@ -0,0 +1,25 @@
+//https://www.lanqiao.cn/courses/31015/learning/?id=1927006&compatibility=false
+#include<iostream>
+using namespace std;
+long long fastPow(long long a,long long n,long long mod)
+{
+  long long ans=1;
+  a%=mod;
+  while(n)
+  {
+    if(n&1==1)//按位与，取二进制最后一位 1011跳过a^4
+    {
+      ans=(ans*a)%mod;//每一步都取模
+    }
+    a=a*a%mod;//二次方
+    n>>=1;//把n右移一位，去掉刚才的最后一位
+  }
+  return ans;
+}
+int main()
+{
+  long long b,p,k;
+  cin>>b>>p>>k;
+  cout<<fastPow(b,p,k)<<endl;
+  return 0;
+}
